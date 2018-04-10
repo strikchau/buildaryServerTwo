4aa041879def220634e9a5a929d8151b9a792397
alan <clp021504@gmail.com>
2018-04-10 18:51:31 +0800

    hi

--- server.js
+++ server.js
@@ -217,7 +217,7 @@ io.on("connection", function(socket){
 				console.log('success');
 				console.log(doc);
 				console.log('Disconnected from MongoDB\n');
-				socket.emit("GETWITHOUTDATA",doc);
+				socket.emit("GETWITHDATA",doc);
 			});
 		});
 	});
